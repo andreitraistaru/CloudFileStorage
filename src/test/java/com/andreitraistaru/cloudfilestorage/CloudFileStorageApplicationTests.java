342fc35b050a8a9c79005154b12cf88fdea5272d
Andrei Trăistaru <andreitraistaru@hotmail.com>
2024-01-08 21:58:32 +0200

    Prepare release

--- src/test/java/com/andreitraistaru/cloudfilestorage/CloudFileStorageApplicationTests.java
+++ src/test/java/com/andreitraistaru/cloudfilestorage/CloudFileStorageApplicationTests.java
@@ -1,11 +1,7 @@
 package com.andreitraistaru.cloudfilestorage;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CloudFileStorageApplicationTests {
-    @Test
-    void contextLoads() {
-    }
 }
